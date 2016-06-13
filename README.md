d95ec82c8c1d8b9e9c088a8a73d9d902591d265e
lbsilver <lb_silver@hotmail.com>
2016-06-13 12:14:22 -0700

    update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Omar Alamoudi Github page
 ### Used js libraries
 * [typed.js](http://www.mattboldt.com/demos/typed-js/)
-* ~~[fullpage.js](http://alvarotrigo.com/fullPage/)~~
+* [fullpage.js](http://alvarotrigo.com/fullPage/)
--- js/main.js
+++ js/main.js
@@ -1,29 +1,59 @@
-$(function(){
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        
+    });
+});
+
+$.getJSON("lang/ar.json",function(lang){
+    console.log($("<meta>").attr("lang",lang.lang));
+});
+
+
+
+$(function() {
+
+    $("#hi").typed({
+        strings: ["Hi", "Welcome to my Github page ^1500"],
+        // stringsElement: $('#typed-strings'),
+        typeSpeed: 40,
+        backDelay: 1000,
+        loop: false,
+        contentType: 'html', // or text
+        // defaults to false for infinite loop
+        loopCount: false,
+        callback: function() { $(".typed-cursor").remove();
+            newTyped(); },
+        resetCallback: function() {}
+    });
+
+    $(".reset").click(function() {
+        $("#typed").typed('reset');
+    });
+
+});
+
+function newTyped() {
+
+    $(function() {
 
         $("#typed").typed({
             // strings: ["Typed.js is a <strong>jQuery</strong> plugin.", "It <em>types</em> out sentences.", "And then deletes them.", "Try it out!"],
             stringsElement: $('#typed-strings'),
-            typeSpeed: 70,
-            backDelay: 1500,
+            typeSpeed: 40,
+            backDelay: 1000,
             loop: false,
             contentType: 'html', // or text
             // defaults to false for infinite loop
             loopCount: false,
-            callback: function(){ foo(); },
+            callback: function() { foo(); },
             resetCallback: function() { newTyped(); }
         });
 
-        $(".reset").click(function(){
+        $(".reset").click(function() {
             $("#typed").typed('reset');
         });
 
     });
+}
 
-    function newTyped(){ /* A new typed object */ }
 
-    function foo(){ console.log("Callback"); }
-
-$.getJSON("lang/ar.json",function(lang){
-    console.log(lang.lang);
-    $("html").attr("lang",lang.lang);
-});
