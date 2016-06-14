c75e1e284c1da47d74425ee364244e3710fdbb6c
lbsilver <lb_silver@hotmail.com>
2016-06-13 19:57:40 -0700

    update

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ var main = function(){
 //            $("html").attr("lang",data.lang);            $('#greeting').append(data.greeting.forEach(parsep));
             $.each(data.greeting, function(i,val){
                 console.log(val);
-                $('#greeting').remove("p");
+                $('#greeting').empty();
                 $('#greeting').append($('<p>').html(val));
             });
             
