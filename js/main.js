a1c2e358106dbb83562f652e92661c2202605f28
lbsilver <lb_silver@hotmail.com>
2016-06-14 12:10:34 -0700

    update

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
 var main = function(){
-    $('#fullpage').fullpage({
-//        anchors:["lang","welcome","projects","contacts"]
-    });
+//    $('#fullpage').fullpage({
+////        anchors:["lang","welcome","projects","contacts"]
+//    });
 
+    $(".main").onepage_scroll({
+   sectionContainer: "section",     // sectionContainer accepts any kind of selector in case you don't want to use section
+   easing: "ease-in",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
+                                    // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
+   animationTime: 1000,             // AnimationTime let you define how long each section takes to animate
+   pagination: true,                // You can either show or hide the pagination. Toggle true for show, false for hide.
+   updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
+   beforeMove: function(index) {},  // This option accepts a callback function. The function will be called before the page moves.
+   afterMove: function(index) {},   // This option accepts a callback function. The function will be called after the page moves.
+   loop: false,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
+   keyboard: true,                  // You can activate the keyboard controls
+   responsiveFallback: false,        // You can fallback to normal page scroll by defining the width of the browser in which
+                                    // you want the responsive fallback to be triggered. For example, set this to 600 and whenever
+                                    // the browser's width is less than 600, the fallback will kick in.
+   direction: "vertical"            // You can now define the direction of the One Page Scroll animation. Options available are "vertical" and "horizontal". The default value is "vertical".  
+});
 
     $(".langbtn").click(function(){
         
@@ -12,9 +28,9 @@ var main = function(){
         } else if($(this).html() =="عربي"){
             var langfile= "lang/ar.json"
             console.log(langfile);
-       };
+       }
         $.getJSON(langfile,function(data){
-            $("html").attr("lang",data.lang);          
+            $('html').attr('lang',data.lang);          
             $('#greeting').empty();
             $.each(data.greeting, function(i,val){
                 console.log(val);
@@ -36,6 +52,7 @@ var main = function(){
             var $version;
             $.each(data.projects, function(i,val){
                 $project = $('<div>').attr('class','slide');
+                $project.attr('data-anchor',val.title);
                 $title = $('<h2>').html(val.title);
                 $description = $('<p>').html(val.description);
                 $links = $('<div>').attr('id','links');
@@ -50,7 +67,7 @@ var main = function(){
                 });
                 $status=$('<div>').text(val.status);
                 $version=$('<div>').text(val.version);
-                $('#projects').append(project);
+                $('#projects').append($project);
             });
         });
     });
