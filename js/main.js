74ae4e475ae34ee66527d3187c94aac2fa341fca
lbsilver <lb_silver@hotmail.com>
2016-06-22 19:05:49 -0700

    update

--- js/main.js
+++ js/main.js
@@ -146,7 +146,7 @@ var build = function(data,option=""){
         
         // GitHub Fork me ribbon
         
-        $('.forkme').attr('src','https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67');
+        $('.forkme').attr('src','images/forkme_ar.png');
         $('.forkme').attr('data-canonical-src','https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png');
         $('.forkme').css({"position": "absolute", "top": "0", "right": "0", "border": "0"});
         
